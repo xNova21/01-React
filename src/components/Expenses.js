1eef329b069358c32df43e57afe37539eb5c458c
xNova21 <luciazgz93@gmail.com>
2022-02-02 12:34:37 +0100

    React basics

---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  let expenses = props.expenses;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        date={expenses[1].date}
+        amount={expenses[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        date={expenses[2].date}
+        amount={expenses[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        date={expenses[3].date}
+        amount={expenses[3].amount}
+      ></ExpenseItem>
+    </div>
+  );
+};
+export default Expenses;
